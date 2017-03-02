61e9fa0250901b29a021b18392e1ff9a70f910e2
Annie Sonna and Levi Bibo <annie+levi@>
2017-03-01 16:05:08 -0800

    correct the env variable template to hide API key

--- src/EmailExperiment/Models/EnvironmentVariablesTemplate.cs
+++ src/EmailExperiment/Models/EnvironmentVariablesTemplate.cs
@@ -7,8 +7,9 @@ namespace EmailExperiment.Models
 {
     public class EnvironmentVariables
     {
-        public static string MailgunKey = "key-af37c2cef9f3c7fe4bfebc8aafad13f6";
-        public static string MailgunDomain = "sandbox3d0d3e7a1e19472b84f6fc69100450e2.mailgun.org";
-        public static string GmailClientId = "503154617510-q58nnv5agui9q9tkbls4dqqkvojk8vpe.apps.googleusercontent.com";
+        public static string MailgunKey = "{{MAILGUN-API-KEY}}";
+        public static string MailgunDomain = "{{MAILGUN-DOMAIN}}";
+        public static string GmailClientId = "{{GMAIL-CLIENT-ID}}"; //Not working yet
+        
     }
 }
